20a40501122de7c92b8ff8466bbc394a135727e3
Ravin <99ravin99@gmail.com>
2026-09-21 23:30:14 +0500

    implement transaction form

--- src/App.tsx
+++ src/App.tsx
@@ -2,51 +2,67 @@ import './App.css';
 import Balance from './components/Balance';
 import Header from './components/Header';
 import type { Transaction } from './types/transaction';
-import TransactionItem from './components/TransactionItem';
 import TransactionList from './components/TransactionList';
 import TransactionForm from './components/TransactionForm';
-
-const transactions: Transaction[] = [
-  {
-    id: '1',
-    type: 'expense',
-    amount: 2500,
-    category: 'Еда',
-    description: 'Продукты',
-    date: '2026-09-20',
-    isPlanned: false,
-  },
-  {
-    id: '2',
-    type: 'income',
-    amount: 150000,
-    category: 'Зарплата',
-    description: 'Зарплата за сентябрь',
-    date: '2026-09-20',
-    isPlanned: true,
-  },
-  {
-    id: '3',
-    type: 'expense',
-    amount: 1200,
-    category: 'Развлечения',
-    description: 'Кино',
-    date: '2026-09-19',
-    isPlanned: false,
-  },
-];
+import { useState } from 'react';
 
 function App() {
+  const [transactions, setTransactions] = useState<Transaction[]>([
+    {
+      id: '1',
+      type: 'expense',
+      amount: 2500,
+      category: 'Еда',
+      description: 'Продукты',
+      date: '2026-09-20',
+      isPlanned: false,
+    },
+    {
+      id: '2',
+      type: 'income',
+      amount: 150000,
+      category: 'Зарплата',
+      description: 'Зарплата за сентябрь',
+      date: '2026-09-20',
+      isPlanned: true,
+    },
+    {
+      id: '3',
+      type: 'expense',
+      amount: 1200,
+      category: 'Развлечения',
+      description: 'Кино',
+      date: '2026-09-19',
+      isPlanned: false,
+    },
+  ]);
+
+  const totalExpenses = transactions
+    .filter((transaction) => transaction.type === 'expense')
+    .reduce((total, transaction) => total + transaction.amount, 0);
+
+  const totalIncomes = transactions
+    .filter((transaction) => transaction.type === 'income')
+    .reduce((total, transaction) => total + transaction.amount, 0);
+
+  function addTransaction(transaction: Transaction): void {
+    setTransactions([...transactions, transaction]);
+  }
+
   return (
     <>
       <Header />
 
       <main>
-        <Balance income={150000} expenses={2500} balance={147500} />
+        <Balance
+          income={totalIncomes}
+          expenses={totalExpenses}
+          balance={totalIncomes - totalExpenses}
+        />
 
         <TransactionList transactions={transactions} />
 
-        <TransactionForm />
+        <TransactionForm onAddTransaction={addTransaction} />
       </main>
     </>
   );
--- src/components/TransactionForm.tsx
+++ src/components/TransactionForm.tsx
@@ -1,35 +1,89 @@
-function TransactionForm() {
+import type { Transaction } from '../types/transaction';
+import { useState } from 'react';
+
+type TransactionFormProps = {
+  onAddTransaction: (transaction: Transaction) => void;
+};
+
+function TransactionForm({ onAddTransaction }: TransactionFormProps) {
+  const [amount, setAmount] = useState('');
+  const [category, setCategory] = useState('');
+  const [description, setDescription] = useState('');
+  const [date, setDate] = useState('');
+  const [isPlanned, setIsPlanned] = useState(false);
+  const [type, setType] = useState<'income' | 'expense'>('expense');
+
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        const newTransaction: Transaction = {
+          id: crypto.randomUUID(),
+          type,
+          amount: Number(amount),
+          category,
+          description,
+          date,
+          isPlanned,
+        };
+
+        onAddTransaction(newTransaction);
+      }}
+    >
       <h2>Добавить операцию</h2>
 
       <div>
-        <button type="button">Доход</button>
-        <button type="button">Расход</button>
+        <button type="button" onClick={() => setType('income')}>
+          Доход
+        </button>
+        <button type="button" onClick={() => setType('expense')}>
+          Расход
+        </button>
       </div>
 
       <label>
         Сумма
-        <input type="number" />
+        <input
+          type="number"
+          value={amount}
+          onChange={(event) => setAmount(event.target.value)}
+        />
       </label>
 
       <label>
         Категория
-        <input type="text" />
+        <input
+          type="text"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
       </label>
 
       <label>
         Описание
-        <input type="text" />
+        <input
+          type="text"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
       </label>
 
       <label>
         Дата
-        <input type="date" />
+        <input
+          type="date"
+          value={date}
+          onChange={(event) => setDate(event.target.value)}
+        />
       </label>
 
       <label>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={isPlanned}
+          onChange={(event) => setIsPlanned(event.target.checked)}
+        />
         Запланированная операция
       </label>
 
